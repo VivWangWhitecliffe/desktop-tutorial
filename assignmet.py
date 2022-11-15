17f9b3522fb9f3b1f2af14faca6f5a24418ebb9f
Vivian Wang <VivianW@whitecliffe.ac.nz>
2022-11-15 16:14:26 +1300

    delete comments
    I deleted Line 81 as an example

--- assignmet.py
+++ assignmet.py
@@ -78,6 +78,6 @@ class BowlingGame:
 		
 
 #Your tasks for code parts:
-#1: If there are any bugs in the code, you have to remove using debugging and run the project and test cases.
+
 #2: Refactor the code (Improve its structure without changing external behaviour).
 #3: Report everything using github commits and versioning control.
